d4ead99ca0eed07b1af16a11def9849c4b8799c0
MaryHal <maryhal@users.noreply.github.com>
2014-05-26 20:24:29 -0700

    ItemGenerator class

---
+++ src/fp/infiniteset/dungeonBot/ItemGenerator.java
@@ -0,0 +1,24 @@
+package fp.infiniteset.dungeonBot;
+
+import java.io.FileNotFoundException;
+import java.util.Random;
+
+public class ItemGenerator
+{
+    private WordCloud adjectives;
+    private Random rng;
+
+    public ItemGenerator() throws FileNotFoundException
+    {
+        adjectives = new WordCloud("adjectives");
+        rng = new Random();
+    }
+
+    public Weapon makeWeapon()
+    {
+        return new Weapon(adjectives.getRandomWord() + " Sword",
+                rng.nextInt(3),
+                rng.nextInt(2));
+    }
+}
+
--- src/fp/infiniteset/dungeonBot/Weapon.java
+++ src/fp/infiniteset/dungeonBot/Weapon.java
@@ -2,5 +2,11 @@ package fp.infiniteset.dungeonBot;
 
 public class Weapon extends Item
 {
+    public Weapon(String name, int mod, int power)
+    {
+        this.baseName = name;
+        this.modifier = mod;
+        this.power = power;
+    }
 }
 
